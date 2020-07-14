cdae288d43de3d467f17489a171e8546befe1d7b
Nikhil Ameria <nikhil.ameria@agreeya.net>
2020-07-14 12:05:14 +0530

    index css file removed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import history from './history'
 import configureStore from './store/configureStore'
 
 import '../node_modules/flexboxgrid/dist/flexboxgrid.min.css'
-import './index.css';
 
 const store = configureStore()
 
